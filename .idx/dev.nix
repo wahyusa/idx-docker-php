7b432bb8406148d5f1aa44a387e247987d18cedd
wahyusa <27491616+wahyusa@users.noreply.github.com>
2025-04-24 19:07:28 +0700

    Initial commit

---
+++ .idx/dev.nix
@@ -0,0 +1,12 @@
+{ pkgs, … }: {
+  channel = "stable-24.05";
+
+  # 1) Docker CLI & Compose
+  packages = [
+    pkgs.docker            # Docker Engine client
+    pkgs.docker-compose    # Docker Compose v1
+  ];
+
+  # 2) Enable the Docker daemon
+  services.docker.enable = true;
+}
